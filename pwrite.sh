88f800142deba0a7860597f563d8157b5560a600
Abhinaba Basu <abhinab@microsoft.com>
2018-07-19 19:20:58 -0700

    Parallel write

---
+++ pwrite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Specify the number of cores"
+    exit 1
+fi
+
+for i in $(seq 1 $1)
+do
+    echo "running $i"
+    `taskset -c $i writefile.exe /hana/logbackups/$i.txt 1000 20 1>~/$i.txt 2>&1 &`
+done
