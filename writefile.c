8e362c3f4ed58acbfc03f2b22eef34ad2b9d5e0a
Abhinaba Basu <abhinab@microsoft.com>
2018-07-20 02:10:37 -0700

    write in smaller blocks

--- writefile.c
+++ writefile.c
@@ -7,44 +7,49 @@ char* GetMemoryBuffer(const unsigned int size)
     srand(time(NULL));
     char* b = (char *)malloc(size * sizeof(char));
     unsigned int loc = 0;
-    while (loc < size) {
+    while (loc < size)
+    {
         char ch = 'A' + (char)(rand() %26);
         b[loc++] = ch;
     }
 
     return b;
-
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc < 4)
+    if (argc < 3)
     {
-        printf("Usage: writefile <path> <count of mb> <number of times>\n");
+        printf("Usage: writefile <path> <run time in seccond>\n");
         return -1;
     }
     
     char *fname = argv[1];
-    unsigned int nData = atoi(argv[2]);
-    unsigned int count = atoi(argv[3]);
+    float runtime = atof(argv[2]);
 
-    printf("Writing to %s, %u times\n", fname, count);
-    const unsigned int size = nData * 1024*1024;
+    printf("Writing to %s, %f seconds\n", fname, runtime);
+    const unsigned int size = 8 * 1024;
     
     printf("Generating random data\n");
     char *pMem = GetMemoryBuffer(size);
 
+    printf("Starting write\n");
     FILE *fp = fopen(fname, "w");
-    for(unsigned int i = 0; i < count; ++i)
+    clock_t begin = clock();
+    float timeTaken; 
+    do
     {
-        printf("Writing %2u: ", i);
-        clock_t begin = clock();
-        fwrite(pMem, sizeof(char), size, fp);
-        fflush(fp);
+        for(unsigned int i = 0; i < 8; ++i)
+        {
+            fwrite(pMem, sizeof(char), size, fp);
+            fflush(fp);
+        }
+
         clock_t end = clock();
-        printf("took %f sec\n", (double)(end - begin) / CLOCKS_PER_SEC);
+        timeTaken = (float)(end - begin) / CLOCKS_PER_SEC;
         fseek(fp, 0, SEEK_SET);
-    }
+
+    } while (timeTaken < runtime);
 
     fclose(fp);
     free(pMem);
